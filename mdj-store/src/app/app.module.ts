93e35386d139b754c89d9e6c47f12aa70b79eb41
t4rget <taron.jonaa@hotmail.com>
2023-12-07 21:35:18 -0300

    se implemento PRODUCTS y el FOOTER

--- mdj-store/src/app/app.module.ts
+++ mdj-store/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { NewsComponent } from './shared/components/news/news.component';
+import { ProductsComponent } from './shared/components/products/products.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewsComponent
+    NewsComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
