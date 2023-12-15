660738b774326232ef11a20d7c1d3212449e1fc3
t4rget <taron.jonaa@hotmail.com>
2023-12-14 23:45:08 -0300

    se corriguio products

--- mdj-store/src/app/pages/products/products-routing.module.ts
+++ mdj-store/src/app/pages/products/products-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+//const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+//  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class ProductsRoutingModule { }
